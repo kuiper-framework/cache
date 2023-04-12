5e04ac6434b0deae1cca84f6b3a21ce29a34777c
Ye Wenbin <yewenbin@winwin-inc.com>
2023-04-12 18:01:49 +0800

    add cache implement˘

---
+++ src/stash/InvalidArgumentException.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Kuiper package.
+ *
+ * (c) Ye Wenbin <wenbinye@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace kuiper\cache\stash;
+
+class InvalidArgumentException extends \InvalidArgumentException implements \Psr\Cache\InvalidArgumentException
+{
+}
